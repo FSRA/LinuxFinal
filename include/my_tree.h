4da2b364a63d913f28db1844882c21e64fc03487
FSRA <1169659587@qq.com>
2017-05-22 21:52:58 +0800

    Create my_tree.h

---
+++ include/my_tree.h
@@ -0,0 +1,20 @@
+#ifndef _MY_TREE_H
+#define _MY_TREE_H
+
+typedef	int	ElemType;			//数据类型
+typedef	int Status;			//返回值类型
+
+//定义二叉树结构
+typedef struct BiTNode{
+	ElemType	data;					//数据域
+	struct BiTNode	*lChild, *rChlid;	//左右子树域
+}BiTNode, *BiTree;
+
+Status CreateBiTree(BiTree *T)
+void TraverseBiTree(BiTree T)
+void InOrderBiTree(BiTree T)
+void PostOrderBiTree(BiTree T)
+int TreeDeep(BiTree T)
+int Leafcount(BiTree T,int num)
+
+#endif //_MY_TREE_H
