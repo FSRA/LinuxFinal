9b1809cce3de0d65e8f56ef3cd18897e5a8f5e08
FSRA <1169659587@qq.com>
2017-05-22 18:46:02 +0800

    Create my_queue.h

---
+++ include/my_queue.h
@@ -0,0 +1,10 @@
+#ifndef _MY_QUEUE_H
+#define _MY_QUEUE_H
+
+Status InitQueue(LinkQueue *q)
+Status EnQueue(LinkQueue *q,ElemType e)
+Status DeQueue(LinkQueue *q,ElemType *e)
+Status Display(LinkQueue *q)
+Status PrintfQueue(LinkQueue *Q)
+
+#endif //_MY_QUEUE_H
