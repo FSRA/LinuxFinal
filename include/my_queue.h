3f3b3a90209d91f5860fff8c3c13e7824c4eb29e
FSRA <1169659587@qq.com>
2017-05-23 08:32:54 +0800

    Update my_queue.h

--- include/my_queue.h
+++ include/my_queue.h
@@ -1,6 +1,23 @@
 #ifndef _MY_QUEUE_H
 #define _MY_QUEUE_H
 
+#define ElemType int
+#define Status int
+#define OK 1
+#define ERROR 0
+
+typedef struct QNode
+{
+    ElemType data;
+    struct QNode *next;
+}QNode;
+
+typedef struct LinkQueue
+{
+    QNode *front;
+    QNode *rear;
+}LinkQueue;
+
 Status InitQueue(LinkQueue *q)
 Status EnQueue(LinkQueue *q,ElemType e)
 Status DeQueue(LinkQueue *q,ElemType *e)
